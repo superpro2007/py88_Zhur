b8e782954673ffe82e34c4ffe318ed41b7a73987
Yauhen Zhur <evgeni1897@gmail.com>
2023-06-03 14:02:32 +0300

    add food message matching

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -1,7 +1,26 @@
+import re
+
 import telebot
+
 import config
 
+REGEX = r"(\w+\s*)+\n\d+\n\d+\n\d+"
+
 bot = telebot.TeleBot(config.TG_BOT_TOKEN, parse_mode='HTML')
+
+
+def is_text_matches(message: telebot.types.Message) -> bool:
+    result = re.match(REGEX, message.text, re.MULTILINE)
+    if result is None:
+        return False
+    else:
+        return True
+
+
+@bot.message_handler(func=is_text_matches)
+def handle_food(message: telebot.types.Message):
+    print(message.text)
+    bot.reply_to(message=message, text='eda sohranena')
 
 
 @bot.message_handler(func=lambda m: True)

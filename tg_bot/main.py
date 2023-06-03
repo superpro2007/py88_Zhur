b21277e2d512fbd2cedb79f0af6dfeb0c296c799
Yauhen Zhur <evgeni1897@gmail.com>
2023-06-03 13:24:35 +0300

    add invite message

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -4,14 +4,25 @@ import config
 bot = telebot.TeleBot(config.TG_BOT_TOKEN, parse_mode='HTML')
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message: telebot.types.Message):
-    bot.send_message(chat_id=message.from_user.id, text='darova chel')
-
-
 @bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+def invite(message):
+    text = '''
+vvedite edy v formate:
+
+imya edi
+koli4estvo belkov
+koli4estvo zhirov
+koli4estvo uglevodov
+
+
+primer:
+
+yabloko
+1
+1
+10
+'''
+    bot.send_message(chat_id=message.from_user.id, text= text)
 
 
 bot.infinity_polling()

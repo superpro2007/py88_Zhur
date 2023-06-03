36e1e001e358dc7cc0af9622390533ff90b9c450
Yauhen Zhur <evgeni1897@gmail.com>
2023-06-03 18:39:29 +0300

    add message parsing

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -4,7 +4,7 @@ import telebot
 
 import config
 
-REGEX = r"(\w+\s*)+\n\d+\n\d+\n\d+"
+REGEX = r"(((\w+\s*)+)\n)((\d+)\n)((\d+)\n)(\d+)"
 
 bot = telebot.TeleBot(config.TG_BOT_TOKEN, parse_mode='HTML')
 
@@ -17,10 +17,26 @@ def is_text_matches(message: telebot.types.Message) -> bool:
         return True
 
 
+def parse_food(text: str) -> dict:
+    text_parts = re.findall(REGEX, text, re.MULTILINE)[0]
+    name_product = text_parts[1]
+    proteins = text_parts[4]
+    fats = text_parts[6]
+    carbohydrates = text_parts[7]
+    return {
+        'name': name_product,
+        'proteins': proteins,
+        'fats': fats,
+        'carbohydrates': carbohydrates
+    }
+
+
 @bot.message_handler(func=is_text_matches)
 def handle_food(message: telebot.types.Message):
-    print(message.text)
+    food = parse_food(message.text)
+    food['user_id'] = message.from_user.id
     bot.reply_to(message=message, text='eda sohranena')
+    print(food)
 
 
 @bot.message_handler(func=lambda m: True)

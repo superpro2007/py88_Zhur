4ce3a22c1d1c9c2060a62c8a9707739882e36a36
Yauhen Zhur <evgeni1897@gmail.com>
2023-06-04 16:09:35 +0300

    remove bot

--- food_server/config.py
+++ food_server/config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
 
-TG_BOT_TOKEN = os.getenv('TG_BOT_TOKEN')
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')

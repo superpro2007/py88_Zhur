0eeb9454e470506d0853c2cb6f0d0b7e833b59f1
Yauhen Zhur <evgeni1897@gmail.com>
2023-06-04 18:34:04 +0300

    add get food

--- food_server/app.py
+++ food_server/app.py
@@ -3,12 +3,17 @@ from flask import request
 import storage
 app = Flask(__name__)
 
+
 @app.get('/food')
 def food_get():
-    return 'Hello world'
+    user_id = int(request.args['user_id'])
+    response = storage.get_food(user_id)
+    return response
+
 
 @app.post('/food')
 def food_post():
     for food in request.json:
         storage.save_to_db(food)
     return 'ok'
+

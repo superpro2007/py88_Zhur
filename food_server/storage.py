128fd85afd3682a0f1155f1000d664ad2ca71842
Yauhen Zhur <evgeni1897@gmail.com>
2023-06-04 18:34:04 +0300

    add get food

--- food_server/storage.py
+++ food_server/storage.py
@@ -13,3 +13,22 @@ def save_to_db(food: dict):
     cur.close()
     conn.close()
 
+def get_food(user_id: int)->[dict]:
+    conn = psycopg2.connect(f"dbname={config.DB_NAME} user={config.DB_USER} password={config.DB_PASSWORD} "
+                            f"host={config.DB_HOST} port={config.DB_PORT}")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM food WHERE user_id = %s", (user_id,))
+    food_list = cur.fetchall()
+    response_food = []
+    for food in food_list:
+        response_food.append({
+            'name': food[1],
+            'proteins': food[2],
+            'fats': food[3],
+            'carbohydrates': food[4],
+            'created_at': food[5],
+            'user_id': food[6]
+        })
+    cur.close()
+    conn.close()
+    return response_food

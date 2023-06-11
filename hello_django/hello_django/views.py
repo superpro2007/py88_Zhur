8a73fc8bce09e3b33c118c39c4fdc16c7ed36371
Yauhen Zhur <evgeni1897@gmail.com>
2023-06-11 14:31:28 +0300

    add hello template

--- hello_django/hello_django/views.py
+++ hello_django/hello_django/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse, HttpRequest
 from django.views import View
+from django.shortcuts import render
 
 
 class HelloView(View):
     def get(self, request: HttpRequest):
-        return HttpResponse('Hello world')
+        return render(request, 'hello.html')

1a80b7e115f14a760e1db5139c5caa00f9673ab1
Yauhen Zhur <evgeni1897@gmail.com>
2023-06-11 15:20:56 +0300

    add hello world

--- hello_django/hello_django/views.py
+++ hello_django/hello_django/views.py
@@ -6,6 +6,16 @@ from django.shortcuts import render
 class HelloView(View):
     def get(self, request: HttpRequest):
         color = {
-            'color': 'red'
+            'color': 'red',
+            'text': 'hello!'
+        }
+        return render(request, 'hello.html', context=color)
+
+
+class HelloWorldView(View):
+    def get(self, request: HttpRequest):
+        color = {
+            'color': 'blue',
+            'text': 'hello world!'
         }
         return render(request, 'hello.html', context=color)

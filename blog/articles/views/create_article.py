84ddecfa2e443b188b2f7631b8877e996e733490
Yauhen Zhur <evgeni1897@gmail.com>
2023-06-18 21:41:30 +0300

    format

--- blog/articles/views/create_article.py
+++ blog/articles/views/create_article.py
@@ -3,14 +3,16 @@ from django.shortcuts import render
 from django.views import View
 from django.shortcuts import redirect
 from articles.models import Article
+
+
 class CreateArticle(View):
     def get(self, request: HttpRequest) -> HttpResponse:
-        return render(request, 'create_article.html')
+        return render(request, "create_article.html")
 
     def post(self, request: HttpRequest) -> HttpResponse:
-        title = request.POST.get('title')
-        text = request.POST.get('text')
+        title = request.POST.get("title")
+        text = request.POST.get("text")
         article = Article(title=title, text=text)
         article.save()
-        self.request.session['message'] = 'Success!'
-        return redirect('home')
+        self.request.session["message"] = "Success!"
+        return redirect("home")
--- blog/articles/views/home.py
+++ blog/articles/views/home.py
@@ -1,4 +1,3 @@
-
 from django.http import HttpResponse, HttpRequest
 from django.views import View
 from django.shortcuts import render
@@ -8,10 +7,9 @@ from articles.models import Article
 class Home(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         articles = Article.objects.all()
-        titles = list(map(lambda article: article.title,  articles))
+        titles = list(map(lambda article: article.title, articles))
         titles.reverse()
-        message = self.request.session['message']
-        context = {'articles': titles, 'message': message}
-        self.request.session['message'] = ''
-        return render(request, 'home.html', context=context)
-
+        message = self.request.session["message"]
+        context = {"articles": titles, "message": message}
+        self.request.session["message"] = ""
+        return render(request, "home.html", context=context)

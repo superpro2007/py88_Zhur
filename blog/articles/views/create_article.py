059e4ae6f198daee421052559166c1a7bc2d7115
Yauhen Zhur <evgeni1897@gmail.com>
2023-06-18 20:20:10 +0300

    add save to db

--- blog/articles/views/create_article.py
+++ blog/articles/views/create_article.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from django.views import View
 from django.shortcuts import redirect
-
+from articles.models import Article
 class CreateArticle(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(request, 'create_article.html')
@@ -10,5 +10,6 @@ class CreateArticle(View):
     def post(self, request: HttpRequest) -> HttpResponse:
         title = request.POST.get('title')
         text = request.POST.get('text')
-
+        article = Article(title=title, text=text)
+        article.save()
         return redirect('home')

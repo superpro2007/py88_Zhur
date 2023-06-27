57a2c1a25a6497d128b9d69633b50cca2e4ec129
Yauhen Zhur <evgeni1897@gmail.com>
2023-06-27 22:10:04 +0300

    add post request handling

--- auto_rent/auto/views.py
+++ auto_rent/auto/views.py
@@ -6,3 +6,11 @@ from django.http import HttpResponse, HttpRequest
 class CreateView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(request, "create_auto.html")
+
+    def post(self, request: HttpRequest) -> HttpResponse:
+        car_brand = request.POST.get("car_brand", "")
+        car_model = request.POST.get("car_model", "")
+        vin = request.POST.get("vin", "")
+        print(f"{car_brand}, {car_model}, {vin}")
+
+        return render(request, "create_auto.html")
